f6fcc369b93f7f943992fedd108240e1ff8d371f
Jiwon Yum <79418036+xx10222@users.noreply.github.com>
2021-09-10 12:53:56 +0900

    안경잡이 개발자 코테 강의 - 2강 선택정렬
    선택정렬 코드
    시간복잡도 : O(N^2) 으로 비효율적이다

---
+++ glass_class/selection_sort.cpp
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+// 선택 정렬(selection sort)
+int main()
+{
+    int array[10]={1,10,5,8,7,6,4,3,2,9};
+    int index, temp;
+    int min = 99999;
+    for(int i=0;i<10;i++)
+    {
+        for(int j=i;j<10;j++)
+        {
+            if(min>array[j])
+            {
+                min=array[j];
+                index=j;
+            }
+        }
+        temp=array[i];
+        array[i]=array[index];
+        array[index]=temp;
+    }
+    for(int i=0;i<10;i++)
+    {
+        printf("%d ", array[i]);
+    }
+    return 0;
+
+    return 0;
+}
