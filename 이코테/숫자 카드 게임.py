58cb20bbee1b65c3f5093c133c0e7406da2bc2a0
Jiwon Yum <79418036+xx10222@users.noreply.github.com>
2021-12-03 02:34:35 +0900

    그리디 숫자 카드 게임 파이썬 코드
    min 함수를 사용하여 구현

---
+++ 이코테/숫자 카드 게임.py
@@ -0,0 +1,16 @@
+#n,m을 공백으로 구분하여 입력받기
+n, m = map(int, input().split())
+
+result=0
+
+#한 줄씩 입력받아 확인
+for i in range(n):
+    data=list(map(int, input().split()))
+
+    #현재 행에서 가장 작은 수 찾기
+    min_value=min(data)
+
+    #가장 작은 수 중에서 가장 큰 수 찾기
+    result = max(min_value, result)
+
+print(result)
